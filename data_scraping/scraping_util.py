20607e79dd1e85e09c1ecdce6c00665c380d0057
Madeline <millerita07@gmail.com>
2024-09-05 14:15:21 +0300

    encoding issues fixed def for sure (delusional)

--- data_scraping/scraping_util.py
+++ data_scraping/scraping_util.py
@@ -16,9 +16,23 @@ def request_page(url):
     if response.status_code != 200:
         LOG.warning(f'Failed to fetch page {url}')
         return
-    response.encoding = response.apparent_encoding
 
-    soup = BeautifulSoup(response.content, 'html.parser')
+    content_type = response.headers.get('Content-Type')
+    if content_type:
+        # Check if 'charset' is specified in the Content-Type header
+        content_type_parts = content_type.split('charset=')
+        if len(content_type_parts) > 1:
+            # Set the encoding to the server-specified charset
+            server_encoding = content_type_parts[1].strip()
+            response.encoding = server_encoding
+        else:
+            # Fallback to apparent encoding if charset isn't specified
+            response.encoding = response.apparent_encoding
+    else:
+        # Fallback to apparent encoding if no Content-Type header is available
+        response.encoding = response.apparent_encoding
+
+    soup = BeautifulSoup(response.content.decode(response.encoding, errors='replace'), 'html.parser')
     return soup
 
 
@@ -41,9 +55,23 @@ def request_page_contents(url, tag=None, tag_class=None, tag_id=None):
     if response.status_code != 200:
         LOG.warning(f'Failed to fetch page {url}')
         return
-    response.encoding = response.apparent_encoding
 
-    soup = BeautifulSoup(response.content, 'html.parser')
+    content_type = response.headers.get('Content-Type')
+    if content_type:
+        # Check if 'charset' is specified in the Content-Type header
+        content_type_parts = content_type.split('charset=')
+        if len(content_type_parts) > 1:
+            # Set the encoding to the server-specified charset
+            server_encoding = content_type_parts[1].strip()
+            response.encoding = server_encoding
+        else:
+            # Fallback to apparent encoding if charset isn't specified
+            response.encoding = response.apparent_encoding
+    else:
+        # Fallback to apparent encoding if no Content-Type header is available
+        response.encoding = response.apparent_encoding
+
+    soup = BeautifulSoup(response.content.decode(response.encoding, errors='replace'), 'html.parser')
 
     if tag is None:
         return soup
