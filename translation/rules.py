153762f2e021beae9310eaa1bf9076294f872a74
Madeline <millerita07@gmail.com>
2024-10-15 17:58:57 +0300

    rules fixes and minor changes

--- translation/rules.py
+++ translation/rules.py
@@ -5,29 +5,71 @@ from util.global_logger import GLOBAL_LOGGER as LOG
 
 class UA2USL:
     _auxiliary_verbs = {
-        ('Past', 'Imp') : Word('був'),
+        ('Past', 'Imp')  : Word('був'),
         ('Past', 'Perf') : Word('вже'),
-        ('Fut', 'Imp') : Word('буде'),
-        ('Fut', 'Perf') : Word('потім'),
+        ('Fut', 'Imp')   : Word('буде'),
+        ('Fut', 'Perf')  : Word('потім'),
     }
+
+    @staticmethod
+    def _no_punctuation(word):
+        if word.token.is_punct:
+            if word.is_left_child:
+                word.parent.children_left.remove(word)
+                return True
+            elif word.is_right_child:
+                word.parent.children_right.remove(word)
+                return True
+            else:
+                #LOG.error(f'Word {str(word)} isn\'t assigned as a child. Can\'t remove root.')
+                #LOG.warning('Punctuation mark was assigned as root. Is the sentence empty?')
+                word.text = ''
+
+    @staticmethod
+    def _remove_parts_of_speech(word):
+        if word.token.pos_ in ['ADP', 'CCONJ', 'SCONJ', 'SYM', 'X']:
+            if word.is_left_child:
+                word.parent.children_left.remove(word)
+                return True
+            elif word.is_right_child:
+                word.parent.children_right.remove(word)
+                return True
+            else:
+                #LOG.warning(f'Word {str(word)} isn\'t assigned as a child. Can\'t remove root.')
+                word.text = ''
 
     def _verb_infinitive(self, word):
         if not hasattr(word.token, 'pos_') or not hasattr(word.token, 'lemma_'):
             return
         if word.token.pos_ == 'VERB':
-            word.text = word.token.lemma_
             morph = word.token.morph.to_dict()
-            tense = morph['Tense']
-            aspect = morph['Aspect']
-            if (tense, aspect) in self._auxiliary_verbs:
-                word.children_right.insert(0, self._auxiliary_verbs[(tense, aspect)])
+            if 'Tense' in morph.keys() and 'Aspect' in morph.keys():
+                tense = morph['Tense']
+                aspect = morph['Aspect']
+                if (tense, aspect) in self._auxiliary_verbs:
+                    word.children_right.insert(0, self._auxiliary_verbs[(tense, aspect)])
 
     @staticmethod
-    def _nominative_case(word):
+    def _lemmatize(word):
         if not hasattr(word.token, 'pos_') or not hasattr(word.token, 'lemma_'):
             return
-        if word.token.pos_ in ['NOUN', 'ADJ', 'NUM', 'PRON']:
-            word.text = word.token.lemma_
+        word.text = word.token.lemma_
+        if word.token.lemma_ == 'рок':
+            word.text = "рік"
+        if '.' in word.text and not word.token.is_punct:
+            word.text = Word(word.token.norm_).token.lemma_
+
+    @staticmethod
+    def _noun_plural_to_noun_noun(word):
+        if word.token.pos_ == 'NOUN':
+            morph = word.token.morph.to_dict()
+            if 'Number' in morph.keys():
+                num = morph['Number']
+                if num == 'Plur':
+                    word.text = word.token.lemma_
+                    child = Word(word.text, parent=word)
+                    child.is_left_child = True
+                    word.children_left.append(child)
 
     @staticmethod
     def _negation_after_negated(word):
@@ -42,53 +84,30 @@ class UA2USL:
                     child.is_right_child = True
 
     @staticmethod
-    def _adjectives_after_object(word):
-        if not word.children_left:
-            return
+    def _subject_verb_object(word):
         for child in word.children_left[:]:
-            if child.token.pos_ == 'ADJ':
+            if child.token.dep_ == 'dobj':
                 word.children_left.remove(child)
                 word.children_right.insert(0, child)
                 child.is_left_child = False
                 child.is_right_child = True
-
-    @staticmethod
-    def _noun_plural_to_noun_noun(word):
-        if word.token.pos_ == 'NOUN':
-            morph = word.token.morph.to_dict()
-            num = morph['Number']
-            if num == 'Plur':
-                word.text = word.token.lemma_
-                child = Word(word.text, parent=word)
+        for child in word.children_right[:]:
+            if child.token.dep_ == 'nsubj':
+                word.children_right.remove(child)
+                word.children_left.insert(0, child)
                 child.is_left_child = True
-                word.children_left.append(child)
+                child.is_right_child = False
 
     @staticmethod
-    def _no_punctuation(word):
-        if word.token.is_punct:
-            if word.is_left_child:
-                word.parent.children_left.remove(word)
-                return True
-            elif word.is_right_child:
-                word.parent.children_right.remove(word)
-                return True
-            else:
-                LOG.error(f'Word {str(word)} isn\'t assigned as either left or right child. \
-                            Can\'t remove punctuation mark.')
-                LOG.warning('Punctuation mark was assigned as root. \
-                             Something went real wrong with spacy idk??')
-
-    @staticmethod
-    def _remove_parts_of_speech(word):
-        if word.token.pos_ in ['ADP', 'CCONJ', 'SCONJ']:
-            if word.is_left_child:
-                word.parent.children_left.remove(word)
-                return True
-            elif word.is_right_child:
-                word.parent.children_right.remove(word)
-                return True
-            else:
-                LOG.error(f'Word {str(word)} isn\'t assigned as either left or right child. Can\'t remove word.')
+    def _adjectives_after_object(word):
+        if not word.children_left:
+            return
+        for child in word.children_left[:]:
+            if child.token.pos_ == 'ADJ' or child.token.dep_ == 'amod':
+                word.children_left.remove(child)
+                word.children_right.insert(0, child)
+                child.is_left_child = False
+                child.is_right_child = True
 
     @staticmethod
     def _question_at_sentence_end(word):
@@ -99,12 +118,12 @@ class UA2USL:
         if pron_type in ['Int']:
             if word.is_left_child:
                 word.parent.children_left.remove(word)
+                word.sentence.root.children_right.append(Word(word.text, parent=word.sentence.root))
             elif word.is_right_child:
                 word.parent.children_right.remove(word)
+                word.sentence.root.children_right.append(Word(word.text, parent=word.sentence.root))
             else:
-                LOG.error('Word isn\'t assigned as either left or right child. Can\'t remove word.')
-
-            word.sentence.root.children_right.append(word)
+                word.sentence.root_at_end = True
 
     def translate(self, sentence):
         def _rule_order(word):
@@ -114,11 +133,12 @@ class UA2USL:
             if self._remove_parts_of_speech(word):
                 return
             # word form
+            self._lemmatize(word)
             self._verb_infinitive(word)
-            self._nominative_case(word)
-            # TODO possibly just convert everything to lemmas
             self._noun_plural_to_noun_noun(word)
             # sentence structure
+            if word is word.sentence.root:
+                self._subject_verb_object(word)
             self._adjectives_after_object(word)
             self._negation_after_negated(word)
             self._question_at_sentence_end(word)
