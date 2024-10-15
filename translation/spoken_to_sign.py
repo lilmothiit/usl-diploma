718b827be00f51aa77d9f909355f9a3955c25acf
Madeline <millerita07@gmail.com>
2024-10-15 17:59:30 +0300

    fixes and func for mass translation

--- translation/spoken_to_sign.py
+++ translation/spoken_to_sign.py
@@ -1,8 +1,66 @@
-from translation.tokens import Sentence
+from translation.tokens import Sentence, nlp
 from translation.rules import RULESET
 
+from util.global_logger import GLOBAL_LOGGER as LOG
+from util.path_resolver import PATH_RESOLVER as REPATH
+from util.annotator import Annotator
 
-if __name__ == '__main__':
+import faulthandler
+import itertools
+import json
+
+
+def line_reader(filepath, skip_n):
+    with open(filepath, 'r', encoding='utf-8') as file:
+        # Skip the first N lines using itertools.islice
+        for line in itertools.islice(file, skip_n, None):
+            yield line.strip()
+
+
+def spoken_to_sign():
+    annotator = Annotator(REPATH.LANG_DATASET_DIR / 'spoken_to_sign.csv', header=['UA', 'USL'], delimiter='\t')
+
+    file_status = {}
+    if REPATH.exists(REPATH.LANG_DATASET_DIR / 'file_status.json'):
+        with open(REPATH.LANG_DATASET_DIR / 'file_status.json', 'r') as f:
+            file_status = json.load(f)
+    save_status = open(REPATH.LANG_DATASET_DIR / 'file_status.json', 'w+')
+
+    for file_path in REPATH.LANG_DATASET_DIR.iterdir():
+        if not file_path.is_file() or not file_path.suffix == '.txt':
+            continue
+        if file_path.name in file_status:
+            if file_status[file_path.name]['complete']:
+                continue
+        else:
+            file_status[file_path.name] = {'complete': False, 'read_lines': 0, 'saved_lines': 0}
+
+        texts = line_reader(file_path, file_status[file_path.name]['read_lines'])
+
+        for doc in nlp.pipe(texts, disable=['senter', 'ner']):
+            file_status[file_path.name]['read_lines'] += 1
+            save_status.seek(0)
+            save_status.write(json.dumps(file_status, indent=4) + '\n')
+
+            if not doc.text.strip():
+                continue
+
+            sent = Sentence(doc)
+            RULESET.translate(sent)
+            text = str(sent)
+
+            if not text or len(text) <= 5:
+                continue
+
+            annotator.record(line=[doc.text.strip(), str(sent).strip()])
+            file_status[file_path.name]['saved_lines'] += 1
+
+            if file_status[file_path.name]['read_lines'] % 100_000 == 0:
+                LOG.info(f'Processed {file_status[file_path.name]["read_lines"]} lines, '
+                         f'Saved {file_status[file_path.name]["saved_lines"]}')
+
+
+def demo():
     test_texts = [
         'Я не довіряю людям.',
         'Я довіряла людям.',
@@ -23,7 +81,7 @@ if __name__ == '__main__':
         'Як ти почуваєшся?',
 
         'Смачний пиріг на тарілці.',
-        'На тарілці мало пирогів.'
+        'На тарілці мало пирогів.',
     ]
 
     test_ground_truth = [
@@ -46,17 +104,20 @@ if __name__ == '__main__':
         'ти почувати себе як',
 
         'тарілка пиріг смачний',
-        'тарілка пиріг мало'
+        'тарілка пиріг мало',
     ]
 
-    #print('ORIGINAL'.ljust(40), end='')
-    #print('TRANSLATED'.ljust(40), end='')
-    #print('TARGET')
+    print('ORIGINAL'.ljust(40), end='')
+    print('TRANSLATED'.ljust(40), end='')
+    print('TARGET')
     for text, gt in zip(test_texts, test_ground_truth):
         sent = Sentence(text)
         RULESET.translate(sent)
         print(text.ljust(40), end='')
         print(str(sent).ljust(40), end='')
         print(gt)
-        #print(sent.root.text, end=' ')
-        #print(sent.root.token.morph)
+
+
+if __name__ == '__main__':
+    faulthandler.enable()
+    spoken_to_sign()
--- translation/tokens.py
+++ translation/tokens.py
@@ -3,7 +3,8 @@ from spacy.tokens import Doc, Token
 from config.config import CONFIG
 
 
-nlp = spacy.load(CONFIG.SPACY_MODEL_NAME)
+spacy.require_gpu()
+nlp = spacy.load(CONFIG.SPACY_MODEL_NAME, disable=['senter', 'ner'])
 
 
 class Word:
@@ -51,12 +52,13 @@ class Sentence:
     def __init__(self, text):
         if isinstance(text, str):
             self.doc = nlp(text)
-        elif isinstance(text, Doc):
+        elif isinstance(text, Doc) or isinstance(text, spacy.tokens.Span):
             self.doc = text
         else:
-            raise TypeError(f"Unexpected type {type(text)}. Expected str or spacy.tokens.Doc.")
+            raise TypeError(f"Unexpected type {type(text)}. Expected str, Doc or Span.")
 
         self.root = None
+        self.root_at_end = False
         nodes = {token.i: Word(token) for token in self.doc}
         for token in self.doc:
             if token.head.i == token.i:  # The root node (a token that points to itself)
@@ -65,6 +67,10 @@ class Sentence:
             else:
                 parent_node = nodes[token.head.i]
                 child_node = nodes[token.i]
+
+                if self._creates_cycle(child_node, parent_node):
+                    continue
+
                 child_node.parent = parent_node
                 child_node.sentence = self
                 if parent_node.token.i < child_node.token.i:
@@ -74,17 +80,73 @@ class Sentence:
                     parent_node.children_left.append(child_node)
                     child_node.is_left_child = True
 
+        self.tree_depth = self._calculate_depth(self.root)
+
     def __repr__(self):
         return f"<Sentence from: {self.doc.text}>"
 
     def __str__(self):
-        return self.string_tree(self.root).strip()
+        return self.text(self.root).strip()
 
-    def string_tree(self, node, level=0):
+    def text(self, node=None, level=0):
+        if node is None:
+            node = self.root
+        if level > self.tree_depth:
+            return ' lol error'
         string = ''
+
         for child in node.children_left:
-            string += self.string_tree(child, level + 1)
-        string += node.text + ' '
+            string += self.text(child, level + 1)
+        if not self.root_at_end or node is not self.root:
+            string += node.text + ' '
         for child in node.children_right:
-            string += self.string_tree(child, level+1)
+            string += self.text(child, level + 1)
+
+        if node is self.root and self.root_at_end:
+            string += self.root.text
         return string
+
+    def text_tree(self, node=None, level=0):
+        if node is None:
+            node = self.root
+        if level >= len(self.doc):
+            return ' lol error'
+
+        string = ''
+        string += ' ' * 4 * level + node.text + '\n'
+        for child in node.children_left:
+            string += self.text_tree(child, level + 1)
+        for child in node.children_right:
+            string += self.text_tree(child, level + 1)
+        return string
+
+    def _creates_cycle(self, potential_ancestor, node):
+        """
+        Perform a depth-first search (DFS) to determine
+        if potential_ancestor is an ancestor of node.
+        """
+        if node is None:
+            return False
+        if node == potential_ancestor:
+            return True
+
+        # Recursively check for cycles in both left and right children
+        for child in node.children_left + node.children_right:
+            if self._creates_cycle(potential_ancestor, child):
+                return True
+
+        return False
+
+    def _calculate_depth(self, node):
+        """
+        Recursively calculate the depth of the tree from the given node.
+        The depth of a node is 1 + the maximum depth of its children.
+        """
+        if not node.children_left and not node.children_right:
+            return 1
+
+        # Calculate depth for both left and right children
+        left_depth = max((self._calculate_depth(child) for child in node.children_left), default=0)
+        right_depth = max((self._calculate_depth(child) for child in node.children_right), default=0)
+
+        return 1 + max(left_depth, right_depth)
